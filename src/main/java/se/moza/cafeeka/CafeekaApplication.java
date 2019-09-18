92df03ae49d72ba45c182defdd43ae3a1474abb6
parvism <parvism@gmail.com>
2019-09-18 13:13:00 +0200

    first commit

---
+++ src/main/java/se/moza/cafeeka/CafeekaApplication.java
@@ -0,0 +1,14 @@
+package se.moza.cafeeka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeekaApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CafeekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/moza/cafeeka/CafeekaApplicationTests.java
@@ -0,0 +1,16 @@
+package se.moza.cafeeka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CafeekaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
